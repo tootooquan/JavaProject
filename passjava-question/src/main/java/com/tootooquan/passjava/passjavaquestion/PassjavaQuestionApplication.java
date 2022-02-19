d9dc03ec9f0ef4b466bb2282ee502884aca39b0c
“ZhangYuquan” <zhangyuquan@seu.edu.cn>
2022-02-19 19:37:08 +0800

    创建了新的子项目并更新了主项目的pom.xml和.gitignore

---
+++ passjava-question/src/main/java/com/tootooquan/passjava/passjavaquestion/PassjavaQuestionApplication.java
@@ -0,0 +1,13 @@
+package com.tootooquan.passjava.passjavaquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassjavaQuestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassjavaQuestionApplication.class, args);
+    }
+
+}
